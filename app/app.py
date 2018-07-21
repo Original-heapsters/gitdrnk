b925d277917f94d0f7088d43e0987610282ecbca
Russell <sell_nat@yahoo.com>
2018-07-21 09:26:17 -0700

    Added mp3 uploader

--- app/app.py
+++ app/app.py
@@ -3,10 +3,14 @@ import random
 import glob
 import pyglet
 from time import gmtime, strftime
-from flask import Flask,render_template, url_for
-from flask import request
+from flask import Flask,render_template, url_for, flash, request, redirect
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './static'
+ALLOWED_EXTENSIONS = set(['mp3'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 staticDir = './static/'
 userInfo = {}
 logFileDir = staticDir + 'logs/'
@@ -53,6 +57,30 @@ def restart():
 def pull():
     os.system('git pull origin master')
     return render_template('index.html')
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload', filename=filename))
+        return render_template('upload.html')
+    else:
+        return render_template('upload.html')
 
 
 @app.route('/precommit/<offender>')
