fe76bf2475d774f4f176f5dd2935404bc9ab9231
Oliver Kim <olive94s@gmail.com>
2018-10-27 14:02:24 -0700

    update post-merge rules for default set

--- app/configuration/rules.py
+++ app/configuration/rules.py
@@ -7,7 +7,7 @@ rule_sets = {
         'post-commit': 'Regular drink.',
         'post-rewrite': 'Finish your current drink. You know what you did.',
         'post-checkout': 'Everyone takes a regular drink.',
-        'post-merge': 'Pick merger and both drink',
+        'post-merge': 'Pick someone and both of you drink',
         'pre-push': 'Drink for how long your push took.',
         'pre-auto-gc': 'OOOF you just got garbage collected, take a drink.',
         'test':'You just ran a test. If youre the first one, everyone else drinks, otherwise you drink'
