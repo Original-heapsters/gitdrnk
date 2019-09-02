20ca129880583f9570458da476f704b2850cf9a5
Russell <sell_nat@yahoo.com>
2019-09-02 00:53:18 -0700

    Removed useless constructor

--- frontend/gitdrnk/src/components/Header.js
+++ frontend/gitdrnk/src/components/Header.js
@@ -3,10 +3,6 @@ import UserBox from './UserBox.js';
 import React from 'react';
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="Header">
