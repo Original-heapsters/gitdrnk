8557fd4fe4b5a192dcc64616d20245e2616b12ac
Russell <sell_nat@yahoo.com>
2019-09-10 20:30:49 -0700

    Corrected port mapping for backend...for the sake of heroku

--- backend/gitdrnk/gitdrnk.py
+++ backend/gitdrnk/gitdrnk.py
@@ -234,4 +234,5 @@ def notify_room(event_json, room_id):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
