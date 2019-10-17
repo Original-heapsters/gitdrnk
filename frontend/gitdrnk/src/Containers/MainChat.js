d5adbbe331b3a3acf9f8326f50d51b1629e17e1a
Russell <sell_nat@yahoo.com>
2019-10-17 00:40:26 -0700

    Arranging the components

--- frontend/gitdrnk/src/Containers/MainChat.js
+++ frontend/gitdrnk/src/Containers/MainChat.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Styles/MainChat.scss'
+import TextEntry from '../components/TextEntry/TextEntry'
+import ActionHolder from '../components/Messages/ActionHolder/ActionHolder'
+import MessageHolder from '../components/Messages/MessageHolder/MessageHolder'
 
 const MainChat = () => {
   return (
     <div className='MainChat'>
+      <div className='ChatWindow'>
+        <ActionHolder/>
+        <ActionHolder/>
+        <MessageHolder/>
+        <ActionHolder claimed='true'/>
+        <MessageHolder/>
+
+      </div>
+      <div className='EntryArea'>
+        <TextEntry/>
+      </div>
     </div>
   )
 }
