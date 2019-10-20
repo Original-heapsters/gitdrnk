56b41e817aee0f8f52e60adbbf452ebd44461afd
Russell <sell_nat@yahoo.com>
2019-10-20 14:24:51 -0700

    Commented out old functionality

--- frontend/gitdrnk/src/App.js
+++ frontend/gitdrnk/src/App.js
@@ -9,11 +9,11 @@ import { joinChat, leaveChat } from './util/SocketHelper.js';
 class App extends Component {
   constructor(props) {
     super(props);
-    var sessionInfo = {
-      gameId: "",
-      email:"",
-      signedIn: false
-    }
+    // var sessionInfo = {
+    //   gameId: "",
+    //   email:"",
+    //   signedIn: false
+    // }
     this.state = {
       server: process.env.REACT_APP_GITDRNK_SVC || "http://localhost:5000",
       profilePicLink:null,
@@ -31,163 +31,172 @@ class App extends Component {
 
 
 
-
-      session: sessionInfo,
-      players: [],
-      chat: [],
-      actions:[],
-      rules: [],
-      games: []
+      //
+      // session: sessionInfo,
+      // players: [],
+      // chat: [],
+      // actions:[],
+      // rules: [],
+      // games: []
     }
     this.handleLogin = this.handleLogin.bind(this);
-    this.updateSession = this.updateSession.bind(this);
-    this.handleNewChat = this.handleNewChat.bind(this);
-    this.handleNewAction = this.handleNewAction.bind(this);
-    this.handleActionUpdate = this.handleActionUpdate.bind(this);
-    this.changeGame = this.changeGame.bind(this);
+    // this.updateSession = this.updateSession.bind(this);
+    // this.handleNewChat = this.handleNewChat.bind(this);
+    // this.handleNewAction = this.handleNewAction.bind(this);
+    // this.handleActionUpdate = this.handleActionUpdate.bind(this);
+    // this.changeGame = this.changeGame.bind(this);
 
+  }
+  handleLogin(gitEmail){
+    console.log(gitEmail)
+
+    // TODO: Send api call to login
+    //       get git username+profile and populate the following
+    this.setState({
+      profilePicLink:'gitdrnk_logo.png',
+      username:gitEmail,
+      email:gitEmail,
+    })
   }
 
   componentDidMount(){
-    getGames((err, gameList)=> {
-      let initialGame = "";
-      if (gameList && gameList.length > 0){
-        initialGame = gameList[0].game_id
-      }
-      let init_games = {
-        gameId: initialGame,
-        email: this.state.session.email,
-        signedIn: false
-      };
-      this.setState({
-        session: init_games,
-        games: gameList
-      });
-    });
+    // getGames((err, gameList)=> {
+    //   let initialGame = "";
+    //   if (gameList && gameList.length > 0){
+    //     initialGame = gameList[0].game_id
+    //   }
+    //   let init_games = {
+    //     gameId: initialGame,
+    //     email: this.state.session.email,
+    //     signedIn: false
+    //   };
+    //   this.setState({
+    //     session: init_games,
+    //     games: gameList
+    //   });
+    // });
   }
 
-  handleLogin(gitEmail){
-    console.log(gitEmail)
-  }
 
-  handleNewChat(err, message) {
-    console.log(message)
-    if (message.type === "join" && message.email === this.state.session.email){
-      getPlayersByGame(message.gameId, (err, playerList)=> {
-        this.setState({players: playerList})
-      });
-    }
-    this.setState({ chat: this.state.chat.concat(message)});
-  }
-
-  handleNewAction(err, action) {
-    if (action.game_id !== this.state.session.gameId){
-      return;
-    }
-    let actionList = this.state.actions.concat(action)
-                      .map(action => {
-                        const existingAudio = action.audio || ""
-                        const audioTarget = this.state.server + "/" + existingAudio;
-                        if (!existingAudio.startsWith(this.state.server)){
-                          action.audio = audioTarget;
-                        }
-                        return action
-                      });
-
-    this.setState({ actions: actionList});
-    var audio = new Audio(action.audio);
-    audio.play();
-  }
-
-  handleActionUpdate(err, action){
-    const existingAudio = action.audio || ""
-    const audioTarget = this.state.server + "/" + existingAudio;
-    if (!existingAudio.startsWith(this.state.server)){
-      action.audio = audioTarget;
-    }
-    const gId = action.gameId || action.game_id;
-    if(this.state.session.gameId === gId){
-      const actionidx = this.state.actions.findIndex( x => x._id === action._id);
-      this.setState({
-        actions: update(this.state.actions,
-          { $splice: [[actionidx, 1, action]] }
-        )
-      });
-    }
-  }
-
-  changeGame(gameSelected){
-    var newSession = {
-      gameId: gameSelected,
-      email:this.state.session.email,
-      signedIn: false
-    }
-    this.setState(
-      {
-        session: newSession,
-        games: [],
-        players: [],
-        chat: [],
-        actions:[],
-        rules: []
-      }
-    );
-  }
-
-  updateSession(uName, email, gId, leave=false){
-    var newSession = {
-      gameId: gId,
-      email:email,
-      signedIn: true
-    }
-    this.setState(
-      {
-        session: newSession,
-        games: [],
-        players: [],
-        chat: [],
-        actions:[],
-        rules: []
-      }
-    );
-
-    if (leave){
-      leaveChat(gId, email);
-      getGames((err, gameList)=> {
-        let initialGame = "";
-        if (gameList && gameList.length > 0){
-          initialGame = gameList[0].game_id
-        }
-        var init_games = {
-          gameId: initialGame,
-          email: this.state.session.email,
-          signedIn: false
-        };
-        this.setState({
-          session: init_games,
-          games: gameList
-        });
-      });
-      return;
-    }
-
-    getRules(gId, (err, ruleset) => {
-      this.setState({rules: ruleset});
-    });
-
-    getActionLog(gId, (err, actionLog) => {
-      if (actionLog && actionLog.length > 0){
-        this.setState({actions: actionLog});
-      }
-    });
-
-    getChatLog(gId, (err, chatLog) => {
-      if (chatLog && chatLog.length > 0){
-        this.setState({chat: chatLog});
-      }
-      joinChat(this.state.session.gameId, this.state.session.email, this.handleNewChat, this.handleNewAction, this.handleActionUpdate);
-    });
-  }
+  //
+  // handleNewChat(err, message) {
+  //   console.log(message)
+  //   if (message.type === "join" && message.email === this.state.session.email){
+  //     getPlayersByGame(message.gameId, (err, playerList)=> {
+  //       this.setState({players: playerList})
+  //     });
+  //   }
+  //   this.setState({ chat: this.state.chat.concat(message)});
+  // }
+  //
+  // handleNewAction(err, action) {
+  //   if (action.game_id !== this.state.session.gameId){
+  //     return;
+  //   }
+  //   let actionList = this.state.actions.concat(action)
+  //                     .map(action => {
+  //                       const existingAudio = action.audio || ""
+  //                       const audioTarget = this.state.server + "/" + existingAudio;
+  //                       if (!existingAudio.startsWith(this.state.server)){
+  //                         action.audio = audioTarget;
+  //                       }
+  //                       return action
+  //                     });
+  //
+  //   this.setState({ actions: actionList});
+  //   var audio = new Audio(action.audio);
+  //   audio.play();
+  // }
+  //
+  // handleActionUpdate(err, action){
+  //   const existingAudio = action.audio || ""
+  //   const audioTarget = this.state.server + "/" + existingAudio;
+  //   if (!existingAudio.startsWith(this.state.server)){
+  //     action.audio = audioTarget;
+  //   }
+  //   const gId = action.gameId || action.game_id;
+  //   if(this.state.session.gameId === gId){
+  //     const actionidx = this.state.actions.findIndex( x => x._id === action._id);
+  //     this.setState({
+  //       actions: update(this.state.actions,
+  //         { $splice: [[actionidx, 1, action]] }
+  //       )
+  //     });
+  //   }
+  // }
+  //
+  // changeGame(gameSelected){
+  //   var newSession = {
+  //     gameId: gameSelected,
+  //     email:this.state.session.email,
+  //     signedIn: false
+  //   }
+  //   this.setState(
+  //     {
+  //       session: newSession,
+  //       games: [],
+  //       players: [],
+  //       chat: [],
+  //       actions:[],
+  //       rules: []
+  //     }
+  //   );
+  // }
+  //
+  // updateSession(uName, email, gId, leave=false){
+  //   var newSession = {
+  //     gameId: gId,
+  //     email:email,
+  //     signedIn: true
+  //   }
+  //   this.setState(
+  //     {
+  //       session: newSession,
+  //       games: [],
+  //       players: [],
+  //       chat: [],
+  //       actions:[],
+  //       rules: []
+  //     }
+  //   );
+  //
+  //   if (leave){
+  //     leaveChat(gId, email);
+  //     getGames((err, gameList)=> {
+  //       let initialGame = "";
+  //       if (gameList && gameList.length > 0){
+  //         initialGame = gameList[0].game_id
+  //       }
+  //       var init_games = {
+  //         gameId: initialGame,
+  //         email: this.state.session.email,
+  //         signedIn: false
+  //       };
+  //       this.setState({
+  //         session: init_games,
+  //         games: gameList
+  //       });
+  //     });
+  //     return;
+  //   }
+  //
+  //   getRules(gId, (err, ruleset) => {
+  //     this.setState({rules: ruleset});
+  //   });
+  //
+  //   getActionLog(gId, (err, actionLog) => {
+  //     if (actionLog && actionLog.length > 0){
+  //       this.setState({actions: actionLog});
+  //     }
+  //   });
+  //
+  //   getChatLog(gId, (err, chatLog) => {
+  //     if (chatLog && chatLog.length > 0){
+  //       this.setState({chat: chatLog});
+  //     }
+  //     joinChat(this.state.session.gameId, this.state.session.email, this.handleNewChat, this.handleNewAction, this.handleActionUpdate);
+  //   });
+  // }
 
   render() {
     return (
